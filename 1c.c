2f3ad7bc632953478c00cb8057709658884d67de
samarth-nagar <samarthxma2009@gmail.com>
2024-03-31 01:08:07 +0530

    backup

--- 1c.c
+++ 1c.c
@@ -6,18 +6,20 @@ char con;
 bool istrue = true; // Initialize istrue to true
 int sqa;
 int main() {
-    printf("s");
     // make an loop
 
     while (istrue) {
-        printf("enter an number = ");
+        printf(" enter an number = ");
         scanf("%d", &input);
 
         int square = input * input;
-        printf("\nsquare is %d \n", square);
+        printf("\n square is %d \n", square);
 
-        printf(
-            "do you want to continue \nfor yes enter y\nfor no enter n \n= ");
+        printf(" do you want to continue"
+               "\n for yes enter [y]"
+               "\n for no enter  [n]"
+               "\n"
+               "\n =");
         scanf(" %c", &con); // Added space before %c to skip whitespace
 
         if (con == 'n') {
--- ls.c
+++ ls.c
@@ -1,5 +1,6 @@
+#include <stdio.h>
 #include <unistd.h>
 int main() {
-    ;
+    printf("hello, world\n");
     ;
 }
