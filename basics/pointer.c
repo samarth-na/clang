c3463b75e04ee532026db999b409f5a959d4026c
samarth-nagar <samarthxma2009@gmail.com>
2024-04-11 02:37:57 +0530

    working on pointers and optimizing other code

--- basics/pointer.c
+++ basics/pointer.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
-int pointer(int a) {
-    int *p = &a;
-    printf("%d", *p);
-    return 0;
-}
 int main() {
-    int a = 10;
-    pointer(a);
+
+    int x = 42;
+    int *ptr = &x;
+
+    printf("%d\n", x);
+    printf("%p\n", &x);
+    printf("%p\n", ptr);
+    printf("%d\n", *ptr);
+
+    int array[] = {1, 2, 3, 4, 5};
+
+    printf("%p\n", &array);
+    printf("%p\n", &array[0]);
+    printf("%p\n", &array[1]);
+    printf("%p\n", &array[2]);
+    printf("%p\n", &array[3]);
+
+    struct structure {
+        int number;
+        char charater;
+    } struct1;
+    printf("%p\n", &struct1.number);
+    printf("%p\n", &struct1);
 }
---
+++ basics/struct.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct node {
+    int Value;
+    struct Node *Next;
+} node;
+
+int name = 5; // value
+int main() {
+    printf("Hello, World!\n");
+    node.Value = 5;
+    struct node two;
+}
