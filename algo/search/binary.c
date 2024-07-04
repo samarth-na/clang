8a2b3e2f855ca646a370514e7548fe46b4c6c858
samarth-nagar <samarthxma2009@gmail.com>
2024-07-05 01:19:53 +0530

    backup

--- algo/search/binary.c
+++ algo/search/binary.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 
+// Binary search function
+// Returns the index of the target if found, otherwise returns -1
 int binarySearch(int arr[], int left, int right, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        printf("left: %d, mid: %d, right: %d\n", left, mid, right);
 
+        // Check if target is present at mid
         if (arr[mid] == target)
-            return mid; // Target found at index mid
+            return mid;
 
+        // If target greater, ignore left half
         if (arr[mid] < target)
-            left = mid + 1; // Target is in the right half
+            left = mid + 1;
+
+        // If target is smaller, ignore right half
         else
-            right = mid - 1; // Target is in the left half
+            right = mid - 1;
     }
 
-    // Target not found
+    // If we reach here, then the element was not present
     return -1;
 }
 
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Main function to demonstrate binary search
 int main() {
-    int arr[] = {1, 3, 5, 7, 9};
+    int arr[] = {2, 3, 4, 10, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 7;
+    int target = 10;
 
-    int index = binarySearch(arr, 0, n - 1, target);
+    printf("Array: ");
+    printArray(arr, n);
 
-    if (index == -1)
-        printf("Target not found in the array\n");
+    int result = binarySearch(arr, 0, n - 1, target);
+    if (result == -1)
+        printf("Element %d is not present in array\n", target);
     else
-        printf("Target found at index %d\n", index);
+        printf("Element %d is present at index %d\n", target, result);
 
     return 0;
 }
